f7bbb1a212be388648f9ef223b35380477db3bd3
Mirko <m.skroch@tu-bs.de>
2017-06-05 17:55:18 +0200

    Sound functionality complete

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour {
+
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioMixerSnapshot quiet;
+    [SerializeField] private AudioMixerSnapshot cave;
+    [SerializeField] private AudioMixerSnapshot action;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider hit) {
+        if (hit.gameObject.tag.Contains("Action")) {
+            action.TransitionTo(1);
+        }
+        if (hit.gameObject.tag.Contains("Cave")) {
+            cave.TransitionTo(1);
+        }
+    }
+
+    void OnTriggerExit(Collider hit) {
+        quiet.TransitionTo(1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
